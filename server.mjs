63536c9164242dfd05a48836a2c062ae0414b7c0
Muhammad Nofil <nofil.khan037@gmail.com>
2023-07-20 21:31:50 +0500

    Working on server

---
+++ server.mjs
@@ -0,0 +1,21 @@
+console.log("This server is express hello world server");
+
+
+import express from 'express';
+const app = express()
+
+// http://:192.168.1.104:3000
+app.get('/', (req, res) => {
+  console.log('Hello World!',new Date());
+    res.send('Hello World!'+new Date())
+})
+app.get('/profile', (req, res) => {
+  console.log('This is profile',new Date());
+    res.send('This is Profile!'+new Date())
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Example server listening on port ${PORT}`)
+})
