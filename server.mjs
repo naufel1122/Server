36d4114867e2bb3039a38f8bb27a17a341270529
Muhammad Nofil <nofil.khan037@gmail.com>
2023-07-21 16:36:02 +0500

    still

--- server.mjs
+++ server.mjs
@@ -10,10 +10,7 @@ app.use(cors())
 
 
 // http://:192.168.1.104:3000
-app.get('/', (req, res) => {
-  console.log('Hello World!', new Date());
-  res.send('Hello World!' + new Date())
-});
+
 app.get('/profile', (req, res) => {
   console.log('This is profile', new Date());
   res.send('This is Profile!' + new Date())
@@ -52,7 +49,7 @@ app.get('/weather/:cityName', (req, res) => {
   }
 });
 
-app.use('/',express.static(path.join(__dirname,'public')))
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 
 const PORT = process.env.PORT || 3000;
